b155669249b54340a445de40da1f9108286c7dde
DEEKSHA AGARWAL <54769217+deekshacse1999@users.noreply.github.com>
2020-06-12 14:41:34 +0530

    Add files via upload

---
+++ ADD.js
@@ -0,0 +1,7 @@
+function add()
+{
+	var n1 = parseInt(document.getElementById("n1").value);
+	var n2 = parseInt(document.getElementById("n2").value);
+	var sum=n1+n2;
+    document.getElementById("n3").value=sum;
+}
